07055724f064f37a5d1f709300f94efbe3933cad
Riddhi <46811067+riddhim@users.noreply.github.com>
2021-10-10 17:26:45 +0530

    Files added

---
+++ app.js
@@ -0,0 +1,49 @@
+// Genre
+function run() {
+    document.getElementById("srt").value = document.getElementById("Ultra").value;
+    var pt = document.getElementById("srt").value;
+    console.log(pt);
+}
+
+function up() {
+    var dop = document.getElementById("srt").value;
+    pop(dop); // Calling function pop
+}
+
+function pop(val) {
+    alert(val);
+}
+
+// Author
+function runp() {
+    document.getElementById("srt1").value = document.getElementById("Ult").value;
+    var pt = document.getElementById("srt1").value;
+    console.log(pt);
+}
+
+function up1() {
+    var dop = document.getElementById("srt1").value;
+    pop(dop); // Calling function pop
+}
+
+function pop(val) {
+    alert(val);
+}
+
+
+
+// Books
+function runpb() {
+    document.getElementById("srt2").value = document.getElementById("Ult1").value;
+    var pt = document.getElementById("srt2").value;
+    console.log(pt);
+}
+
+function up2() {
+    var dop = document.getElementById("srt2").value;
+    pop(dop); // Calling function pop
+}
+
+function pop(val) {
+    alert(val);
+}
